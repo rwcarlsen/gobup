c204fecef9959cab6a4fa4faa3e2f3dfcfc3a6ab
Robert Carlsen <rwcarlsen@gmail.com>
2013-07-04 12:15:43 -0500

    added forgotten rolling pkg

---
+++ rolling/rolling.go
@@ -0,0 +1,69 @@
+package rolling
+
+import (
+	"encoding/binary"
+)
+
+const m = 1 << 16
+
+type RollingSum struct {
+	a      uint
+	b      uint
+	window []byte
+	size   int
+}
+
+func New(window int) *RollingSum {
+	return &RollingSum{size: window}
+}
+
+func (rs *RollingSum) Write(data []byte) (n int, err error) {
+	for _, c := range data {
+		rs.WriteByte(c)
+	}
+	return len(data), nil
+}
+
+func (rs *RollingSum) WriteByte(c byte) error {
+	if len(rs.window) == rs.size {
+		rs.a += -uint(rs.window[0]) + uint(c)
+		rs.b += -uint(rs.size)*uint(rs.window[0]) + rs.a
+	} else if len(rs.window) > 0 {
+		rs.a += uint(c)
+		rs.b += uint(c) * uint(len(rs.window))
+	} else {
+		rs.a = uint(c)
+		rs.b = uint(rs.size) * uint(c)
+	}
+	rs.a %= m
+	rs.b %= m
+
+	rs.window = append(rs.window, c)
+	if len(rs.window) > rs.size {
+		rs.window = rs.window[1:]
+	}
+	return nil
+}
+
+func (rs *RollingSum) Sum32() uint32 {
+	return uint32(rs.a) + uint32(rs.b)*m
+}
+
+func (rs *RollingSum) Size() int {
+	return 4
+}
+
+func (rs *RollingSum) BlockSize() int {
+	return 1
+}
+
+func (rs *RollingSum) Reset() {
+	rs.window = make([]byte, 0)
+	rs.a, rs.b = 0, 0
+}
+
+func (rs *RollingSum) Sum(b []byte) []byte {
+	data := make([]byte, 4)
+	binary.BigEndian.PutUint32(data, rs.Sum32())
+	return append(b, data...)
+}
